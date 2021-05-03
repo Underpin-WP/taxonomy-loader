ec321d396077ad4f3fbed75f57efa14057d53f41
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:25:10 -0700

    Fixes issues where extended plugins did not get registered items.

--- taxonomies.php
+++ taxonomies.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'taxonomies', [
-			'instance' => 'Underpin_Taxonomies\Abstracts\Taxonomy',
-			'default'  => 'Underpin_Taxonomies\Factories\Taxonomy_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy_Instance.php' );
+	$instance->loaders()->add( 'taxonomies', [
+		'instance' => 'Underpin_Taxonomies\Abstracts\Taxonomy',
+		'default'  => 'Underpin_Taxonomies\Factories\Taxonomy_Instance',
+	] );
 } );
