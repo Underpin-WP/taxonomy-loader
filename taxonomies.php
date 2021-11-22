bae7d0f0d8c34e0e4ad44dd80b83e96e9afdd10d
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:06:35 -0600

    Compat for Underpin 2.0.0

--- taxonomies.php
+++ taxonomies.php
@@ -1,14 +1,19 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'taxonomies', [
+	'update' => function ( Underpin $plugin, $args ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Taxonomy_Instance.php' );
-	Underpin\underpin()->get( $file, $class )->loaders()->add( 'taxonomies', [
+	$plugin->loaders()->add( 'taxonomies', [
 		'instance' => 'Underpin_Taxonomies\Abstracts\Taxonomy',
 		'default'  => 'Underpin_Taxonomies\Factories\Taxonomy_Instance',
 	] );
-}, 10, 2 );
+	},
+] ) );
